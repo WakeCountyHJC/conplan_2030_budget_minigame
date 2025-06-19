6cd4b9433ecfd41136b05ce2985a6f9eeb41ba59
WCHJC <wchousingjusticecoalition@gmail.com>
2025-06-19 02:40:36 -0400

    fix: add iPad-specific patches

--- src/index.mjs
+++ src/index.mjs
@@ -138,6 +138,21 @@ function init() {
         }
     );
 
+    document.addEventListener('gesturestart', function(e) {
+        e.preventDefault();
+        document.body.style.zoom = 1;
+    });
+
+    document.addEventListener('gesturechange', function(e) {
+        e.preventDefault();
+        document.body.style.zoom = 1;
+    });
+
+    document.addEventListener('gestureend', function(e) {
+        e.preventDefault();
+        document.body.style.zoom = 1;
+    });
+
     window.addEventListener('resize', () => {
         sceneContainer.reflow();
     });
